995be87710a7e4c7dbdf118b58b5735a68fe9379
Mahdis Asadirad <mahdis.asadirad1384@gmail.com>
2025-04-09 21:50:15 +0330

    print hi my git for three times

--- hiGit.java
+++
@@ -1,7 +0,0 @@
-public class hiGit {
-    public static void main(String[] args) {
-        System.out.println("Hi my git!");
-        System.out.println("Hi my git!");
-        System.out.println("Hi my git!");
-    }
-}
